becdda04609c7c0f8bebacb0e67832f79d230723
David Alberici <dav.alberici@gmail.com>
2021-06-12 08:42:35 +0200

    Added required env variable in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBDDhx0wZEjNRlpPf8U4kQ-9OHsXaH__mo",
+    authDomain: "dalbusco-push-tutorial.firebaseapp.com",
+    projectId: "dalbusco-push-tutorial",
+    storageBucket: "dalbusco-push-tutorial.appspot.com",
+    messagingSenderId: "916585607238",
+    appId: "1:916585607238:web:cb66297f4e7dcbd40ada47",
+    vapidKey: "BP6nOWyPSENyL0j7kg3O4QDECvC9pzIEEmuKpg5EPX1DFgeb8MnBFbNq3RK6ZDoltDAnZKlSxSveEOWWEWWaEXY"
+  }
 };
